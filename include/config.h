dd99c411f0b28a8d14532abacfb6d1790486a423
Art025 <tpag24338@stu.tamagawa.ac.jp>
2026-09-24 12:52:30 +0900

    Add MQTT dashboard

--- include/config.h
+++ include/config.h
@@ -4,6 +4,6 @@
 const char WIFI_SSID[] = "YOUR_WIFI_SSID";
 const char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
 const char MQTT_SERVER[] = "192.168.1.100";
-const char TEAM_ID[] = "TEAM_ID";
+const char TEAM_ID[] = "TEAM_002";
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,22 @@ WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 bool mqttConnectionFailed = false;
 
+void connectMqtt() {
+  if (mqttClient.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT broker...");
+  if (mqttClient.connect(TEAM_ID)) {
+    mqttConnectionFailed = false;
+    Serial.println(" connected");
+  } else {
+    mqttConnectionFailed = true;
+    Serial.print(" failed. State: ");
+    Serial.println(mqttClient.state());
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(500);
@@ -33,13 +49,7 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   mqttClient.setServer(MQTT_SERVER, 1883);
-  if (mqttClient.connect(TEAM_ID)) {
-    Serial.println("MQTT broker connected");
-  } else {
-    Serial.print("MQTT broker connection failed. State: ");
-    Serial.println(mqttClient.state());
-    mqttConnectionFailed = true;
-  }
+  connectMqtt();
 
   Wire.begin(sdaPin, sclPin);
   Wire.setClock(400000);
@@ -68,6 +78,7 @@ void setup() {
 }
 
 void loop() {
+  connectMqtt();
   int status = dht20.read();
 
   if (status == DHT20_OK) {
@@ -86,9 +97,13 @@ void loop() {
       String temperatureTopic = String(TEAM_ID) + "/sensor/temperature";
       String humidityTopic = String(TEAM_ID) + "/sensor/humidity";
 
-      mqttClient.publish(temperatureTopic.c_str(), temperaturePayload.c_str());
-      mqttClient.publish(humidityTopic.c_str(), humidityPayload.c_str());
-      Serial.println("MQTT publish complete");
+      bool temperaturePublished = mqttClient.publish(temperatureTopic.c_str(), temperaturePayload.c_str());
+      bool humidityPublished = mqttClient.publish(humidityTopic.c_str(), humidityPayload.c_str());
+      if (temperaturePublished && humidityPublished) {
+        Serial.println("MQTT publish complete");
+      } else {
+        Serial.println("MQTT publish failed");
+      }
     } else {
       Serial.println("MQTT is not connected. Publish skipped.");
     }
