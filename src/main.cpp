6de91b31b51a8202d0bf2f09a4cd4a35aa847d2b
Art025 <tpag24338@stu.tamagawa.ac.jp>
2026-09-24 11:27:25 +0900

    Add DHT20 sensor reading via I2C

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,33 @@
 #include <Arduino.h>
+#include <DHT20.h>
+#include <Wire.h>
 
-const uint8_t ledPin = 13;
+const uint8_t sdaPin = 4;
+const uint8_t sclPin = 5;
+DHT20 dht20;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  Wire.begin(sdaPin, sclPin);
+
+  if (!dht20.begin()) {
+    Serial.println("DHT20 initialization failed.");
+  }
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
+  int status = dht20.read();
+
+  if (status == DHT20_OK) {
+    Serial.print("Temperature: ");
+    Serial.print(dht20.getTemperature(), 1);
+    Serial.print(" C, Humidity: ");
+    Serial.print(dht20.getHumidity(), 1);
+    Serial.println(" %");
+  } else {
+    Serial.print("DHT20 read failed. Status: ");
+    Serial.println(status);
+  }
+
+  delay(2000);
 }
