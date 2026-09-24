7cc366f2bd15273ccf3496345cef8acec94b14f1
Art025 <tpag24338@stu.tamagawa.ac.jp>
2026-09-24 13:46:01 +0900

    Add MQTT dashboard

--- include/config.h
+++ include/config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char WIFI_SSID[] = "YOUR_WIFI_SSID";
-const char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
-const char MQTT_SERVER[] = "192.168.1.100";
+const char WIFI_SSID[] = "NullPointerException";
+const char WIFI_PASSWORD[] = "tamagawa";
+const char MQTT_SERVER[] = "mqtt.m5stack.com";
 const char TEAM_ID[] = "TEAM_002";
 
 #endif
